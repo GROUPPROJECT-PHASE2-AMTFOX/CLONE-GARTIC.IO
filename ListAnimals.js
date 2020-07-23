c8234482dc66d7ea2fc7790f259292ece53b9b3b
TheodorusArie <theodorusarie@hotmail.com>
2020-07-23 15:59:18 +0700

    list of categories

---
+++ ListAnimals.js
@@ -0,0 +1,7 @@
+let animalList=[
+    'baboon','badger','camel','elephant','cat','caterpillar','catfish','centipede','chameleon','cheetah','beaver','chicken','alligator','chimpanzee','dog','bison','cockroach','cow','crab','crocodille','eagle','deer','fox','elephant','dolphin','donkey','dragonfly','duck','hamster','hedgehog','clam','horse','giraffe','goat','bear','jaguar','jellyfish','koala','lion','lizard','lobster','octopus','ostrich','monkey','mouse','rabbit','panther','raccoon','parrot','peacock','penguin','rat','reindeer','pig','piranha','hippopotamus','tapir','scorpion','tiger','seal','turtle','sheep','seal','snail','snake','sparrow','squid','starfish','swan','wasp','bee','wolf','zebra'
+]
+
+let itemList =[
+    'bed','mug','glass','rug','laptop','computer','handphone','photo','picture','cable','spoon','fork','plate','knife','refrigerator','chair','sofa','wardrobe','shoes','sandals','television','microwave','table','window','door','ruler','pencil','pen','eraser','bag','box','shirt','pillow','clock','tissue','shampoo','soap','toaster','radio','pot','mirror','blender','lamp','sponge','mop','broom','key','faucet','speaker','umbrella','car','book','sink','toilet','brush','desk','fan','grater'
+]
