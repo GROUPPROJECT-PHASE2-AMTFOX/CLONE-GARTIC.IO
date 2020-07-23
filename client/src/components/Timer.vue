f83b65829cd6095d08357d49a7a1413fe54814e2
anggawijaya <anggawijaya004@gmail.com>
2020-07-23 23:40:20 +0700

    timer is ready

---
+++ client/src/components/Timer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>{{ time }}</h2>
+    <button @click="countDownTimer">mulai</button>
+    <h3>{{isChangUser}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Timer",
+  data () {
+    return {
+      time: 5,
+      isChangUser: false,
+    }
+  },
+  methods: {
+    minTime() {
+      this.time = 0
+      this.isChangUser = true
+    },
+    countDownTimer() {
+        window.setInterval(this.minTime, 5000);
+    }
+  }
+}
+</script>
