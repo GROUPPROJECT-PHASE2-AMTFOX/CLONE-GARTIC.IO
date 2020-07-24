17f252fd952ad38ebd14e60ec3b2c1496fb770f5
kvngrldy <kevingrldy@gmail.com>
2020-07-24 14:39:48 +0700

    final-client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/game">Gameroom</router-link> |
-      <router-link to="/login">Login</router-link> | 
-      <router-link to="/lobby">Lobby</router-link>
       <!-- <router-link to="/about">About</router-link> -->
     </div>
     <router-view/>
@@ -22,6 +18,7 @@
 
 #nav {
   padding: 30px;
+  visibility: hidden;
 }
 
 #nav a {
